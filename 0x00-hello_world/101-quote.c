cf5a08e263e0397873e5de810e6699e2c8c95104
Kaleb Demisse <kalebdemisse4@gmail.com>
2023-12-15 04:58:39 +0300

    Task 4, 5, and 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+#include <string.h>
+
+/**
+ * main - prints a string using write
+ *
+ * Return: always 1
+ */
+
+int main(void)
+{
+	char* str;
+	int len;
+
+	str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19,\n";
+	len = strlen(str);
+	write(1, str, len);
+
+	return (1);
+}
