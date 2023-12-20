8618b2eed4820d7adecccbd18b91d7acd904a4f8
Kaleb Demisse <kalebdemisse4@gmail.com>
2023-12-20 05:40:41 +0300

    Advanced Tasks

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - adds to interger
+ * @a: first integer to be added
+ * @b: second integer to be added
+ *
+ * Return: the sum of a and b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_to_98 - print from n to 98
+ * @n: number to start from
+ *
+ * Return: nothing
+ */
+
+void print_to_98(int n)
+{
+	
+}
