77ff89f5d8fee1595e82f50c216850dc95392433
Kaleb Demisse <kalebdemisse4@gmail.com>
2024-01-02 13:20:07 +0300

    Task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - reset an integer to 98
+ * @n: pointer to the value to be resetted
+ *
+ * Return: nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
