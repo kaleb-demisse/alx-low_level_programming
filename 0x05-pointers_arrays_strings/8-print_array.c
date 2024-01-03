bd46a5ac9daf42b36c16d4cca9f8ea781ff431d1
Kaleb Demisse <kalebdemisse4@gmail.com>
2024-01-03 05:13:04 +0300

    Task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,6 +13,11 @@ void print_array(int *a, int n)
 {
 	int i;
 
+	if (n <= 0)
+	{
+		printf("\n");
+		return;
+	}
 	for (i = 0; i < n - 1; ++i)
 		printf("%d, ", *(a + i));
 	printf("%d\n", *(a + i));
