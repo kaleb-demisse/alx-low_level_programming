ef7b61c860370723a25f26451bc60c5e945a1d08
Kaleb Demisse <kalebdemisse4@gmail.com>
2024-01-08 21:35:56 +0300

    Fix Betty

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -13,8 +13,8 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i = 0;
 
-        for (; i < n; ++i)
-                *(dest + i) = *(src + i);
+	for (; i < n; ++i)
+		*(dest + i) = *(src + i);
 
-        return (dest);
+	return (dest);
 }
